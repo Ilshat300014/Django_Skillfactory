0e2fc3e8cde9fc97f11e6152040c07f018d9ca48
Ilshat <ilshatkhabriev@yandex.ru>
2023-02-08 22:32:09 +0300

    D3.2 complete

--- simpleapp/views.py
+++ simpleapp/views.py
@@ -1,4 +1,16 @@
-from django.views import render
+from django.views.generic import ListView, DetailView
+from .models import Product, Category
+
+
+class ProductsList(ListView):
+    model = Product
+    template_name = 'products.html'
+    context_object_name = 'products'
+
+class ProductDetail(DetailView):
+    model = Product
+    template_name = 'product.html'
+    context_object_name = 'product'
 
 # dfv
 # ate your views here.
